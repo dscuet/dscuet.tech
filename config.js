754134f18a6e42086bae9d582ef27f3b2ba3393b
msaaddev <mrsaadirfan@gmail.com>
2019-09-14 20:52:48 +0500

    👌IMPROVE: changed layout text

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'Developer Student Club - UET',
+  subHeading: 'By Google Developers',
   // social
   socialLinks: [
     {
