6ab928ec190b6010769d98d62c6bb61776600107
msaaddev <mrsaadirfan@gmail.com>
2019-09-17 13:09:35 +0500

    👌IMPROVE: update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter SolidState', // <title>
+  siteTitle: 'Developer Student Club - UET | Google Developers', // <title>
   manifestName: 'SolidState',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/images/pic09.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Developer Student Club - UET',
   subHeading: 'By Google Developers',
